43fc515f2fca66cba2c9a161e49caf8015867c79
Lavanyainti <lavanyainti14@gmail.com>
2025-08-18 15:25:12 +0530

    Initial commit

---
+++ routes/addBlogRoute.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const route=express.Router();
+const middleware=require('../Middleware/middleware');
+const { addedBlog, getBlog, getBlogById, getBlogByUser, getBlogByUserToUser, deleteBlog } = require('../controller/addBlogController');
+const multer=require('multer')
+const {CloudinaryStorage}=require('multer-storage-cloudinary')
+const cloudinary=require('../Config/cloudinary')
+const path=require('path');
+
+const storage=new CloudinaryStorage({
+    cloudinary,
+    params:{
+        folder:"blog-image",
+        allowed_formats:["jpg","png","jpeg"]
+    }
+})
+
+const upload=multer({storage});
+
+route.post('/addBlog',middleware,upload.single('thumbnailImage'),addedBlog);
+route.get('/getBlogByUser',middleware,getBlogByUser);
+route.get('/getBlogById/:id',middleware,getBlogById)
+route.get('/getBlog',getBlog)
+route.get('/getBlogByUserToUser/:id',getBlogByUserToUser)
+route.delete('/deleteBlog/:id',deleteBlog)
+module.exports=route;
