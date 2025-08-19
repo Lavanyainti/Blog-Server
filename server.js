b739d1953dfc17abb14d0046691ebe3d8432ac0c
Lavanyainti <lavanyainti14@gmail.com>
2025-08-19 19:10:31 +0530

    fourth commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const profileRoute=require('./routes/profileRoute')
 const path = require('path');
 
 app.use(cors({
-    origin: ["https://whimsical-baklava-7833ab.netlify.app"], 
+    origin: ["https://steady-biscochitos-61738e.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
